a370cabe06b3413162f45f50d5d8047da9debda6
Dennim Olivares <olivares.mtz.den@gmail.com>
2026-04-20 23:22:31 +0000

    En las tablas envez de usar divisores entre filas mejor usar un patron d

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -30,7 +30,7 @@ const TableBody = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
+    className={cn("[&_tr:nth-child(even)]:bg-muted/50", className)}
     {...props}
   />
 ))
@@ -58,7 +58,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "transition-colors data-[state=selected]:bg-muted",
       className
     )}
     {...props}
