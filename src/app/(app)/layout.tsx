b63c0bac3edc27c4ab808b27b463ca82f87fbac2
Dennim Olivares <olivares.mtz.den@gmail.com>
2026-04-25 04:08:47 +0000

    Titulos de las subpaginas y pagina principal no quiero que tengan tanto

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -10,9 +10,9 @@ export default function AppLayout({
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <Sidebar />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-20">
+      <div className="flex flex-col sm:gap-2 sm:py-2 sm:pl-20">
         <Header />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <main className="grid flex-1 items-start gap-4 p-2 sm:px-6 sm:py-0 md:gap-8">
           {children}
         </main>
       </div>
