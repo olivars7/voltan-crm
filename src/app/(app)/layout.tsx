043a549594ac6d785b7c7f10c9c0925dc603f7d2
Dennim Olivares <olivares.mtz.den@gmail.com>
2026-05-01 07:06:28 +0000

    El fondo que sea mas animado y con colores mas pronunciados gradientes i

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Sidebar } from '@/components/layout/Sidebar';
 import { Header } from '@/components/layout/Header';
+import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation';
 
 export default function AppLayout({
   children,
@@ -8,14 +9,25 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen w-full bg-transparent overflow-hidden">
-      <Sidebar />
-      <div className="flex flex-col flex-1 sm:pl-28 sm:pr-8 py-4 h-full">
-        <Header />
-        <main className="flex-1 overflow-y-auto custom-scrollbar pt-2 pr-2">
-          {children}
-        </main>
-      </div>
+    <div className="relative h-screen w-full overflow-hidden">
+      <BackgroundGradientAnimation
+        firstColor="18, 113, 255"
+        secondColor="0, 255, 255"
+        thirdColor="0, 80, 255"
+        fourthColor="0, 200, 255"
+        fifthColor="0, 100, 200"
+        containerClassName="absolute inset-0 z-0"
+      >
+        <div className="relative z-10 flex h-full w-full bg-transparent overflow-hidden">
+          <Sidebar />
+          <div className="flex flex-col flex-1 sm:pl-28 sm:pr-8 py-4 h-full">
+            <Header />
+            <main className="flex-1 overflow-y-auto custom-scrollbar pt-2 pr-2">
+              {children}
+            </main>
+          </div>
+        </div>
+      </BackgroundGradientAnimation>
     </div>
   );
 }
